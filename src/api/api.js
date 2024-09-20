77005d9481bb79aed5a64512cad4b20f66d6783d
VictorFil67 <victorfilinovych@gmail.com>
2024-09-20 18:58:29 +0300

    updated base URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://zenbit-task-back.onrender.com",
-  baseURL: "http://localhost:4000/",
+  baseURL: "https://zenbit-task-back.onrender.com",
+  // baseURL: "http://localhost:4000/",
 });
 
 export function setToken(token) {
